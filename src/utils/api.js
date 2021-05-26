173f5c3bac7a68447a825a6727115a2387be19ac
Anna <adropofwater0@gmail.com>
2021-05-26 13:22:32 +0100

    api functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,21 +2,18 @@ import axios from "axios"
 
 const BASE_URL = 'https://annas-games-reviews.herokuapp.com/api';
 
-// just created for possible future refactoring not used yet
 export const fetchReviews = (category) => {
-    let url =''
-    if (category) {
-        url = `${BASE_URL}/reviews?category=${category}`;
-    } else {
-        url = `${BASE_URL}/reviews`;
-    }
     return axios
-    .get(url)
+    .get(`${BASE_URL}/reviews`, {
+        params: {
+            category: category
+        }
+    })
     .then((response) => response.data.reviews)
 };
 
 export const fetchReviewById = (id) => {
     return axios
     .get(`${BASE_URL}/reviews/${id}`)
-    .then((response) => response.data.reviews);
+    .then((response) => response.data.review);
 };
