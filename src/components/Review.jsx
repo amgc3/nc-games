51d10cb5b740240241d9d87092d1ee5813f21466
Anna <adropofwater0@gmail.com>
2021-05-27 16:02:50 +0100

    Loading...

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { fetchReviewById } from '../utils/api';
-import CommentList from './CommentList'
-
+import CommentList from './CommentList';
+import Counter from './Counter';
 
 const Review = () => {
-    const [review, setReview] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [review, setReview] = useState({});
 
-    const { review_id } = useParams();
-    useEffect(() => {
-        fetchReviewById(review_id)
-        .then((reviewFromApi) => {
-           setReview(reviewFromApi) 
-        })
-    }, [review_id])
+  const { review_id } = useParams();
+  useEffect(() => {
+    fetchReviewById(review_id).then((reviewFromApi) => {
+      setIsLoading(false);
+      setReview(reviewFromApi);
+    });
+  }, [review_id]);
   return (
-    <main> 
-        <h2>
-          {review.title} - {review.owner}
-        </h2>
-        <img src={review.review_img_url} className="li--item--picture" alt="game" />
-        <p>{review.review_body}</p>
-        <p>Comments: {review.comments_count}</p>
-        <CommentList/>
-        <p>Votes: {review.votes}</p>
-        <p>Category: {review.category}</p>
+    <main>
+      {isLoading && <p>Loading...</p>}
+      <h2>
+        {review.title} - {review.owner}
+      </h2>
+      <p>Votes: {review.votes}</p>
+      <Counter votes={review.votes}/>
+      <img
+        src={review.review_img_url}
+        className="li--item--picture"
+        alt="game"
+      />
+      <p>{review.review_body}</p>
+      <p>Comments: {review.comments_count}</p>
+      <CommentList />
+      <p>Category: {review.category}</p>
     </main>
   );
 };
