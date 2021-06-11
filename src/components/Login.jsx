eb613ba0a353cee2bcbb4da622d01c18006f6e6d
Anna <adropofwater0@gmail.com>
2021-06-11 15:00:20 +0100

    added state hooks and some style changes for login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,15 +2,27 @@ import React, {useState} from 'react';
 
 
 const Login = () => {
-    const [username, setUsername] = useState('')
-    const email = "thiandthat@gmail.com"
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+
+    //const email = "thiandthat@gmail.com"
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(username, email)
+    }
     return (
         
-        <form className='Login-form'>
+        <form className='Login-form' onSubmit={handleSubmit}>
             <label htmlFor='username' >Username: </label>
-            <input type='text' id='username' value={username} />
+            <input type='text' id='username' value={username} 
+            onChange={(event) => {
+                setUsername(event.target.value)
+            }} />
             <label htmlFor='email'>Email: </label>
-            <input type='email' id='email' value={email}/>
+            <input type='email' id='email' value={email}
+            onChange={(event) => {
+                setEmail(event.target.value)
+            }} />
             <button className='login-button'>Login</button>
         </form>
     );
